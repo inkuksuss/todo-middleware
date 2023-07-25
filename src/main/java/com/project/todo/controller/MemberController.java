5ec7dbf209abda9dbdb905cec9a2bde750ea8565
inkuksuss <inkuksuss@naver.com>
2023-07-26 00:01:35 +0900

    cors 설정

--- src/main/java/com/project/todo/controller/MemberController.java
+++ src/main/java/com/project/todo/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.project.todo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,10 @@ public class MemberController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @PostMapping("/test")
+    public String test() {
+        return "test";
     }
 }
