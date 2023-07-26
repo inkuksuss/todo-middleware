743735f33cbfd621c372ea149629f814cff0e656
inkuksuss <inkuksuss@naver.com>
2023-07-27 02:19:22 +0900

    도커파일 / 디비 프로필 설정

--- src/main/java/com/project/todo/TodoApplication.java
+++ src/main/java/com/project/todo/TodoApplication.java
@@ -1,10 +1,14 @@
 package com.project.todo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class TodoApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
--- src/main/java/com/project/todo/controller/MemberController.java
+++ src/main/java/com/project/todo/controller/MemberController.java
@@ -1,16 +1,27 @@
 package com.project.todo.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Slf4j
 @RestController
 @RequestMapping("/member")
 public class MemberController {
 
+    @Autowired
+    DataSource dataSource;
+
     @GetMapping("/hello")
-    public String hello() {
+    public String hello() throws SQLException {
+        log.info("datasorure= {}", dataSource.getConnection().getMetaData());
+
         return "hello";
     }
 
