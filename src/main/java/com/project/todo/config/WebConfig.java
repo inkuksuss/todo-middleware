cdf45a4be5a6049659e8169468c3e2dd5f48f907
inkuksuss <inkuksuss@naver.com>
2023-07-28 17:52:37 +0900

    cors 설정 변경

--- src/main/java/com/project/todo/config/WebConfig.java
+++ src/main/java/com/project/todo/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.project.todo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4000")
-                .allowCredentials(true);
+                .allowedOrigins("*")
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name());
     }
 }
